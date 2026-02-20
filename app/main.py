8fbf3e4cb02b0a397f9735bcc460d248ab000c57
ramieeee <rambotty@gmail.com>
2026-02-21 01:02:25 +0900

    Fix CORS

--- app/main.py
+++ app/main.py
@@ -13,6 +13,9 @@ set_log("App started")
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://cr-soles.cloud",
+    "https://www.cr-soles.cloud",
+    "https://cr-soles-eepg6ul8d-ramheeyees-projects.vercel.app",
 ]
 
 app.add_middleware(
